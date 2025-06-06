9719a4213de162f8b7b21861af63c450263a87af
ifeanyi oleforo <ole24001@byui.edu>
2025-06-07 00:00:53 +0100

    more update added

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -8,3 +8,6 @@ function closesidebar(){
     const hide = document.getElementById('ham');
     hide.style.display='none';
 }
+
+const api_key = '69fc43aded5234537ff036a9f4fc41cd';
+const location = 'Onitsha';
